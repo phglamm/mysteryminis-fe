0e79bff4dc35ea0d2d2418c5f139ae5e1aa04b25
phonglam <lamphong1507@gmail.com>
2025-02-12 16:00:36 +0700

    complete ForgotPasswordPage

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,7 +2,8 @@ export const route = {
   home: "/",
   login: "/login",
   register: "/register",
-
+  forgotPassword: "/forgot-password",
+  resetPassword: "/reset-password",
   userManagement: "/userManagement",
   roleManagement: "/roleManagement",
 };
