316eabb7ee60bd28676475263c49c735ac6ebc9f
phonglam <lamphong1507@gmail.com>
2025-02-23 14:02:52 +0700

    commit fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -72,11 +72,10 @@ export default function Header() {
           >
             <motion.div
               whileHover={{ scale: 1.1, color: "red" }}
-              onMouseEnter={() => setIsHovered(true)}
               onClick={navigateToHome}
               whileTap={{ scale: 0.9 }}
             >
-              Collection
+              Home
             </motion.div>
           </motion.span>
 
