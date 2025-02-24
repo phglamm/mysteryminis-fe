8fb3af42eb525a2b60e5f18099a854d9cefc8b22
phonglam <lamphong1507@gmail.com>
2025-02-24 12:59:59 +0700

    swith to local host

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
+import fs from "fs";
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-})
+  plugins: [react(), tailwindcss()],
+  server: {
+    https: {
+      key: fs.readFileSync("localhost-key.pem"),
+      cert: fs.readFileSync("localhost-cert.pem"),
+    },
+    host: "localhost",
+    port: 5173, // Change if needed
+  },
+});
