13ac2724c1e67ee090a85d6cc26a4c6eec56f3ed
MinhLuong30 <minhltse182480@fpt.edu.vn>
2025-03-15 17:03:35 +0700

    fix logic onlineBlindbox

--- src/components/3DBoxModel/BoxModel.jsx
+++ src/components/3DBoxModel/BoxModel.jsx
@@ -1,40 +1,44 @@
-import { Canvas, useFrame } from "@react-three/fiber";
-import { useGLTF, useAnimations, OrbitControls } from "@react-three/drei";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+import { useGLTF, useAnimations, OrbitControls, Environment } from "@react-three/drei";
 import { motion, useSpring } from "framer-motion";
 import { useEffect, useState, useRef } from "react";
 import * as THREE from "three";
-import { Badge, Descriptions, Modal, Rate } from "antd";
-import logo from "../../assets/images/Logo-removebg.png";
-import secretRun from "../../assets/images/secretVid.mp4";
-import commontRun from "../../assets/images/commonVid.mp4";
-import api from "../../config/api";
 import { useSelector } from "react-redux";
 import { selectUser } from "../../Redux/features/counterSlice";
-import { use } from "react";
 import ShowRewardModal from "../ShowRewardModal/ShowRewardModal";
+import { TextureLoader } from "three";
+import api from "../../config/api";
+import bg360 from "../../assets/images/360bg.jpg";
+import secretRun from "../../assets/images/secretVid.mp4";
+import commontRun from "../../assets/images/commonVid.mp4";
 
-const BoxModel = ({ plays, setPlays, onlineSerieBoxId }) => {
+const BoxModel = ({ plays, setPlays, onlineSerieBoxId, fetchBlindBox, showVideo, setShowVideo }) => {
   const [isInteracting, setIsInteracting] = useState(false);
   const [rewardModalVisible, setRewardModalVisible] = useState(false);
-  const [showVideo, setShowVideo] = useState(false);
+
   const [interactionDisabled, setInteractionDisabled] = useState(false);
+  const [showIntro, setShowIntro] = useState(true);
   const [reward, setReward] = useState(null);
   const hasInteracted = useRef(false);
   const videoRef = useRef(null);
   const controlsRef = useRef(null);
   const cameraRef = useRef(null);
-  
 
   const user = useSelector(selectUser);
 
-  // Target position for the camera reset
   const initialCameraPosition = new THREE.Vector3(-150, 20, 0);
   const IntroCameraPosition = new THREE.Vector3(60, 70, 80);
 
-  // UseSpring for smooth transition
   const smoothX = useSpring(initialCameraPosition.x, {});
   const smoothY = useSpring(initialCameraPosition.y, {});
   const smoothZ = useSpring(initialCameraPosition.z, {});
+
+  const texture = useLoader(THREE.TextureLoader, bg360);
+  useEffect(() => {
+    texture.mapping = THREE.EquirectangularReflectionMapping;
+    texture.colorSpace = THREE.SRGBColorSpace;
+  }, [texture]);
+  
 
   useEffect(() => {
     if (!isInteracting && hasInteracted.current) {
@@ -43,21 +47,17 @@ const BoxModel = ({ plays, setPlays, onlineSerieBoxId }) => {
       smoothZ.set(initialCameraPosition.z);
 
       setInteractionDisabled(true);
-      setTimeout(() => {
-        setInteractionDisabled(false);
-      }, 1000);
+      setTimeout(() => setInteractionDisabled(false), 1000);
     }
   }, [isInteracting]);
 
   const unboxOnlineBlindBox = async () => {
     setReward(null);
     try {
-      const payload = {
-        userId: user.userId, // Replace with the actual userId if available
-        onlineSerieBoxId: onlineSerieBoxId, // Replace with the actual onlineSerieBoxId
-      };
-
-      const response = await api.post(`online-serie-box/unbox`, payload);
+      const response = await api.post(`online-serie-box/unbox`, {
+        userId: user.userId,
+        onlineSerieBoxId: onlineSerieBoxId,
+      });
       if (response && response.data) {
         setReward(response.data);
       } else {
@@ -67,33 +67,31 @@ const BoxModel = ({ plays, setPlays, onlineSerieBoxId }) => {
       console.error("Error unboxing online blind box:", error);
     }
   };
+
   useEffect(() => {
     if (plays) {
       unboxOnlineBlindBox();
     }
   }, [plays]);
-  console.log("Unbox Response:", reward);
-
-  const [hasShownIntro, setHasShownIntro] = useState(false);
 
   const CameraAnimation = () => {
     useEffect(() => {
-      if (!hasShownIntro && cameraRef.current) {
-        cameraRef.current.position.copy(IntroCameraPosition); // Start at IntroCameraPosition
-
+      if (cameraRef.current && showIntro) {
+        cameraRef.current.position.copy(IntroCameraPosition);
         setTimeout(() => {
           smoothX.set(initialCameraPosition.x);
           smoothY.set(initialCameraPosition.y);
           smoothZ.set(initialCameraPosition.z);
-          setHasShownIntro(true); // Ensure it runs only once
-        }, 1500); // Delay before moving to initialCameraPosition
+        }, 1500);
+        setShowIntro(false);
       }
-    }, [hasShownIntro]);
+    }, []);
+
     useFrame(() => {
       if (!isInteracting && cameraRef.current) {
         cameraRef.current.position.lerp(
           new THREE.Vector3(smoothX.get(), smoothY.get(), smoothZ.get()),
-          0.02
+          0.009  
         );
         cameraRef.current.lookAt(0, 0, 0);
       }
@@ -120,14 +118,15 @@ const BoxModel = ({ plays, setPlays, onlineSerieBoxId }) => {
       }
     }, [plays, actions]);
 
-    return <primitive object={scene} />;
+    return <primitive object={scene} scale={[13, 13, 13]} />;
   };
 
   return (
     <div className="w-full h-full">
       <Canvas
-        camera={{ position: [-150, 20, 0], fov: 4 }}
-        onCreated={({ camera }) => {
+        camera={{ position: [-150, 20, 0], fov: 40 }}
+        onCreated={({ scene, camera }) => {
+          scene.background = texture;
           cameraRef.current = camera;
         }}
       >
@@ -136,8 +135,8 @@ const BoxModel = ({ plays, setPlays, onlineSerieBoxId }) => {
 
         <OrbitControls
           ref={controlsRef}
-          enableZoom={!interactionDisabled}
-          enablePan={!interactionDisabled}
+          enableZoom={false}
+          enablePan={false}
           enableRotate={!interactionDisabled}
           onStart={() => {
             if (!interactionDisabled) {
@@ -157,22 +156,24 @@ const BoxModel = ({ plays, setPlays, onlineSerieBoxId }) => {
           ref={videoRef}
           autoPlay
           muted
-          className="fixed top-0 left-0 w-full h-full object-cover z-10"
+          className="fixed top-0 left-0 w-full h-full object-cover z-20"
           onPlay={() => {
-            setTimeout(() => {
-              setRewardModalVisible(true);
-            }, 3000);
+            setTimeout(() => setRewardModalVisible(true), 3000);
           }}
         >
-          <source src={reward.isSecret ? secretRun : commontRun} type="video/mp4" />
+          <source src={reward?.isSecret ? secretRun : commontRun} type="video/mp4" />
         </video>
       )}
 
       <ShowRewardModal 
         reward={reward} 
         visible={rewardModalVisible} 
-        onClose={() => setRewardModalVisible(false)} 
-        setShowVideo={setShowVideo} 
+        onClose={() => { 
+          setRewardModalVisible(false); 
+          setShowVideo(false); 
+          fetchBlindBox();
+        }}
+        
       />
     </div>
   );
--- src/pages/UserPages/OnlineBlindBox/OnlineBlindBox.jsx
+++ src/pages/UserPages/OnlineBlindBox/OnlineBlindBox.jsx
@@ -1,69 +1,108 @@
-import InfiniteScroll from "../../../components/React_Bits/InfiniteScroll/InfiniteScroll";
-import gift from "../../../assets/images/OpenedGift.webm";
-
-
 import { useCallback, useEffect, useState } from "react";
-import SplashCursor from "../../../components/React_Bits/SplashCursor/SplashCursor";
-import BoxModel from "../../../components/3DBoxModel/BoxModel";
-import RollingGallery from "../../../components/React_Bits/RollingGallery/RollingGallery";
-import { Button, Card } from "antd";
 import { useParams } from "react-router-dom";
+import { Button } from "antd";
 import api from "../../../config/api";
+import BoxModel from "../../../components/3DBoxModel/BoxModel";
+import Logo from "../../../assets/images/Logo-removebg.png";
 
 const OnlineBlindBox = () => {
   const [plays, setPlays] = useState(false);
   const [blindbox, setBlindBox] = useState([]);
-  const { packageId } = useParams(); 
+  const [loading, setLoading] = useState(true); // New loading state
+  const [showVideo, setShowVideo] = useState(false);
+  const { packageId } = useParams();
   console.log("Selected Box ID from URL:", packageId);
 
   const fetchBlindBox = useCallback(async () => {
+    setLoading(true); // Set loading to true before fetching
     try {
       const response = await api.get(`online-serie-box/${packageId}`);
       setBlindBox(response.data);
       console.log(response.data);
     } catch (error) {
       console.error(error);
+    } finally {
+      setLoading(false); // Set loading to false after fetching
     }
   }, [packageId]);
 
   useEffect(() => {
     fetchBlindBox();
-  }, [plays, fetchBlindBox]);
-  
-  return (
-    <div className="pt-[15%] h-screen justify-center items-center text-center p-[5%] bg-amber-400  flex flex-col">
-      {/* <SplashCursor/> */}
+  }, [fetchBlindBox]);
 
-      {/* Package Details */}      
-      {blindbox.boxOption ? (
-        <div className="flex flex-col items-center w-full">
-          <div className="flex flex-col items-center -mb-[8%]">
-            <div className="text-4xl font-bold">{blindbox.boxOption.boxOptionName}</div>
-            <div>{blindbox.brandDtoResponse.brandName}</div>
-          </div>
-          
-          <div className=" flex justify-center h-[75vh] w-[100%]">
-          <div className="h-[100%] w-[30%] bg-blue-600">
-            hi
-          </div>
-          <div className="h-[100%] w-[40%] ">
-            {blindbox.onlineSerieBoxId && (
-              <BoxModel plays={plays} setPlays={setPlays} onlineSerieBoxId={blindbox.onlineSerieBoxId} />
-            )}
-            <div className="-mt-[20%]">
-              <Button onClick={() => setPlays(!plays)}>{blindbox.boxOption.displayPrice}</Button>
+  return (
+    <div className="lg:pt-[6.8%] pt-[12%] h-screen justify-center items-center text-center flex flex-col">
+      {loading ? ( // Show loading indicator if loading is true
+        <h1>Loading...</h1>
+      ) : (
+        blindbox.boxOption && (
+          <div className="flex bg-black flex-col h-full justify-center items-center w-full">
+            <div className="flex justify-center h-full w-full relative">
+              <div className="w-[50%] flex justify-center items-center h-[100%] absolute z-10 ">
+                <img src={Logo} alt="Logo" className="w-[50%] h-fit" />
+              </div>
+              {/* Left Section */}
+              <div className="h-full flex gap-5 flex-col justify-center items-center w-[30%] relative z-40">
+                {blindbox.boxItemResponseDtos.slice(0,3).map((item, index) => (
+                  <img
+                    key={index}
+                    src={item.imageUrl}
+                    alt={item.boxItemName}
+                    className="w-[80%] bg-red-200 h-[25%] "
+                  />
+                ))}
+              </div>
+
+              {/* Center Section */}
+              <div className="h-fit justify-between flex flex-col pt-[5%] w-[40%] relative z-40">
+                <div>
+                  <div className="text-4xl z-40 font-bold">
+                    {blindbox.boxOption.boxOptionName}
+                  </div>
+                  <div className="z-40">
+                    {blindbox.brandDtoResponse.brandName}
+                  </div>
+                </div>
+              </div>
+
+              {/* Button - Foreground */}
+              <div className="absolute bottom-20 z-40">
+                <Button onClick={() => setPlays(!plays)}>
+                  {blindbox.boxOption.displayPrice}
+                </Button>
+              </div>
+
+              <div className="h-full w-full absolute z-30 flex flex-col items-center">
+                {/* BoxModel - Background Layer */}
+                {blindbox.onlineSerieBoxId && (
+                  <div className="absolute top-0 left-0 w-full h-full z-10">
+                    <BoxModel
+                      plays={plays}
+                      setPlays={setPlays}
+                      onlineSerieBoxId={blindbox.onlineSerieBoxId}
+                      fetchBlindBox={fetchBlindBox}
+                      showVideo={showVideo}
+                      setShowVideo={setShowVideo}
+                    />
+                  </div>
+                )}
+              </div>
+
+              {/* Right Section */}
+              <div className="h-full flex gap-5 flex-col justify-center items-center w-[30%] relative z-40">
+                {blindbox.boxItemResponseDtos.slice(-3).map((item, index) => (
+                  <img
+                    key={index}
+                    src={item.imageUrl}
+                    alt={item.boxItemName}
+                    className="w-[80%] bg-red-200 h-[25%] "
+                  />
+                ))}
+              </div>
             </div>
           </div>
-          <div className="h-[100%] w-[30%] bg-blue-600">
-
-          </div>
-          </div>
-        </div>
-        
-      ) : (
-        <h1>Loading...</h1>
+        )
       )}
-      
     </div>
   );
 };
