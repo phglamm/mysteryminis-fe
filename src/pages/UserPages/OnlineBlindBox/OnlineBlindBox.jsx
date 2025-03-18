a842347efc8b62eb7e7c860a68a47c357ffeaa73
MinhLuong30 <minhltse182480@fpt.edu.vn>
2025-03-18 19:51:40 +0700

    Update OnlineBlindBox.jsx

--- src/pages/UserPages/OnlineBlindBox/OnlineBlindBox.jsx
+++ src/pages/UserPages/OnlineBlindBox/OnlineBlindBox.jsx
@@ -13,7 +13,8 @@ const OnlineBlindBox = () => {
   const [loading, setLoading] = useState(true); // New loading state
   const [showVideo, setShowVideo] = useState(false);
   const { packageId } = useParams();
-  const user = useSelector(selectUser)
+  const [numberBlindBoxItem, setNumberBlindBoxItem] = useState()
+  const user = useSelector(selectUser);
   console.log("Selected Box ID from URL:", packageId);
 
   const fetchBlindBox = useCallback(async () => {
@@ -21,7 +22,7 @@ const OnlineBlindBox = () => {
     try {
       const response = await api.get(`online-serie-box/${packageId}`);
       setBlindBox(response.data);
-      console.log(response.data);
+      setNumberBlindBoxItem(response.data.boxItemResponseDtos?.length || 0);
     } catch (error) {
       console.error(error);
     } finally {
@@ -31,7 +32,10 @@ const OnlineBlindBox = () => {
 
   useEffect(() => {
     fetchBlindBox();
+    setNumberBlindBoxItem(blindbox.boxItemResponseDtos ? blindbox.boxItemResponseDtos.length : 0);
   }, [fetchBlindBox]);
+
+  const firstHalfBlindBoxItem = Math.ceil(numberBlindBoxItem / 2);
 
   const paymentHandler = async () => {
     try {
@@ -42,7 +46,7 @@ const OnlineBlindBox = () => {
         shippingFee: 0,
         voucherId: null,
         paymentMethod: "VNPAY", // Modify as needed
-     // Replace with actual address ID if required   
+        // Replace with actual address ID if required
         discountAmount: 0,
         orderItemRequestDto: [
           {
@@ -55,9 +59,9 @@ const OnlineBlindBox = () => {
           },
         ],
       };
-  
+
       console.log("Sending payment request:", JSON.stringify(requestData));
-  
+
       const response = await api.post("Payment/make-Payment", requestData);
       console.log("Payment successful:", response.data);
       window.location.assign(response.data);
@@ -66,6 +70,7 @@ const OnlineBlindBox = () => {
       console.error("Payment failed:", error);
     }
   };
+
 
   return (
     <div className="lg:pt-[6.8%] pt-[12%] h-screen justify-center items-center text-center flex flex-col">
@@ -79,32 +84,38 @@ const OnlineBlindBox = () => {
                 <img src={Logo} alt="Logo" className="w-[50%] h-fit" />
               </div>
               {/* Left Section */}
-              <div
-                className={`h-full flex gap-5 flex-col justify-center items-center w-[30%] relative ${
-                  showVideo ? "z-10" : "z-40"
-                }`}
-              >
-                {blindbox.boxItemResponseDtos
-                  ?.slice(0, 3)
-                  .map((item, index) => (
-                    <div key={index} className="items-center gap-3 w-[80%] rounded-3xl bg-white/70 border-2 h-[25%] flex flex-row">
-                      <div className=" bg-gray-500/70 h-[25%] rounded-3xl w-[80%] z-40 absolute"></div>
+                      <div
+                      className={`h-full flex gap-5 flex-col justify-center items-center w-[30%] relative ${
+                        showVideo ? "z-10" : "z-40"
+                      }`}
+                      >
+                      {blindbox.boxItemResponseDtos
+                        ?.slice(0, firstHalfBlindBoxItem)
+                        .map((item, index) => (
+                        <div
+                          key={index}
+                          className={`items-center gap-3 w-[80%] rounded-3xl ${
+                          item?.isSecret ? "bg-gradient-to-r from-amber-200 to-yellow-500" : "bg-white/70"
+                          }  border-2 flex flex-row`}
+                          style={{ height: `${100 / firstHalfBlindBoxItem}%` }}
+                          
+                        >
+                          <div className=" items-center px-3 gap-3 w-[100%] rounded-3x h-[100%] flex flex-row">
+                          <img
+                            src={item.imageUrl}
+                            alt={item.boxItemName}
+                            className=" bg-red-200 w-[40%] h-[70%] relative z-30 rounded-xl"
+                          />
+                          <div className="text-center z-30 relative px-5 w-[60%] font-bold truncate">
+                            {item.boxItemName}
 
-                      <div className=" items-center px-3 gap-3 w-[100%] rounded-3x h-[100%] flex flex-row">
-                        <img
-                          src={item.imageUrl}
-                          alt={item.boxItemName}
-                          className=" bg-red-200 w-[40%] h-[70%] relative z-30 rounded-xl"
-                        />
-                        <div className="text-center z-30 relative px-5 w-[60%] font-bold truncate">
-                          {item.boxItemName}
+                          </div>
+                          </div>
                         </div>
+                        ))}
                       </div>
-                    </div>
-                  ))}
-              </div>
 
-              {/* Center Section */}
+                      {/* Center Section */}
               <div className="h-fit justify-between flex flex-col pt-[4%] w-[40%] relative z-40">
                 <div>
                   <div className="text-4xl z-40 font-bold">
@@ -146,10 +157,10 @@ const OnlineBlindBox = () => {
                 }`}
               >
                 {blindbox.boxItemResponseDtos
-                  ?.slice(3, 6)
+                  ?.slice(firstHalfBlindBoxItem)
                   .map((item, index) => (
                     <div
-                      className=" items-center px-3 gap-3 w-[80%] rounded-3xl bg-white/70 border-2 h-[25%] flex flex-row"
+                      className={`items-center px-3 gap-3 w-[80%] rounded-3xl ${item?.isSecret ? "bg-gradient-to-r from-amber-200 to-yellow-500" : "bg-white/70" } border-2 h-[25%] flex flex-row`} 
                       key={index}
                     >
                       <img
