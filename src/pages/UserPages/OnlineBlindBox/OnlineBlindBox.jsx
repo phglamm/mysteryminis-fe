ffdda4fe6f99581493a2b62e7caf3dab0d044c20
MinhLuong30 <minhltse182480@fpt.edu.vn>
2025-03-21 10:16:09 +0700

    Update OnlineBlindBox.jsx

--- src/pages/UserPages/OnlineBlindBox/OnlineBlindBox.jsx
+++ src/pages/UserPages/OnlineBlindBox/OnlineBlindBox.jsx
@@ -6,7 +6,8 @@ import BoxModel from "../../../components/3DBoxModel/BoxModel";
 import Logo from "../../../assets/images/Logo-removebg.png";
 import { useSelector } from "react-redux";
 import { selectUser } from "../../../Redux/features/counterSlice";
-
+import { motion } from "framer-motion";
+import { UpSquareOutlined } from "@ant-design/icons";
 const OnlineBlindBox = () => {
   const [plays, setPlays] = useState(false);
   const [blindbox, setBlindBox] = useState([]);
@@ -15,6 +16,7 @@ const OnlineBlindBox = () => {
   const { packageId } = useParams();
   const [numberBlindBoxItem, setNumberBlindBoxItem] = useState();
   const user = useSelector(selectUser);
+ 
   console.log("Selected Box ID from URL:", packageId);
   const isPlay = useLocation().state?.isPlay;
   const fetchBlindBox = useCallback(async () => {
@@ -80,7 +82,13 @@ const OnlineBlindBox = () => {
   return (
     <div className="lg:pt-[6.8%] pt-[12%] h-screen justify-center items-center text-center flex flex-col">
       {loading ? ( // Show loading indicator if loading is true
-        <h1>Loading...</h1>
+        <div className="flex bg-black flex-col h-full justify-center items-center w-full">
+            <div className="flex justify-center h-full w-full relative">
+              <div className="w-[50%] flex justify-center items-center h-[100%] absolute z-10">
+                <img src={Logo} alt="Logo" className="w-[50%] h-fit" />
+              </div>
+            </div>
+          </div>
       ) : (
         blindbox.boxOption && (
           <div className="flex bg-black flex-col h-full justify-center items-center w-full">
@@ -104,7 +112,7 @@ const OnlineBlindBox = () => {
                           ? "bg-gradient-to-r from-amber-200 to-yellow-500"
                           : "bg-white/70"
                       }  border-2 flex flex-row`}
-                      style={{ height: `${100 / firstHalfBlindBoxItem}%` }}
+                      style={{ height: `${100 / firstHalfBlindBoxItem}%`, maxHeight: "25%" }}
                     >
                       <div className=" items-center px-3 gap-3 w-[100%] rounded-3x h-[100%] flex flex-row">
                         <img
@@ -121,7 +129,7 @@ const OnlineBlindBox = () => {
               </div>
 
               {/* Center Section */}
-              <div className="h-fit justify-between flex flex-col pt-[4%] w-[40%] relative z-40">
+              <div className="h-fit justify-between flex flex-col pt-[2%] w-[40%] relative z-40">
                 <div>
                   <div className="text-4xl z-40 font-bold">
                     {blindbox.boxOption.boxOptionName}
@@ -129,14 +137,31 @@ const OnlineBlindBox = () => {
                   <div className="z-40">
                     {blindbox.brandDtoResponse?.brandName}
                   </div>
+                  <div className="flex flex-row justify-center gap-2 z-40">
+                    <div className={`text-sm px-3 py-1 rounded-md ${blindbox.isSecretOpen ? "bg-gray-500" : "bg-gradient-to-r from-amber-200 to-yellow-500" }`}>
+                      {blindbox.isSecretOpen ? 'Secret Opened' : 'Secret'}
+                    </div>
+                    <div className={`text-sm px-3 gap-1 py-1 items-center justify-center flex rounded-md ${blindbox.isSecretOpen ? "bg-white border-2" : blindbox.priceIncreasePercent > 20 ? "bg-gradient-to-r from-red-500 to-orange-500" : "bg-gradient-to-b from-sky-400 to-sky-200" }`}>
+                      <UpSquareOutlined /> {blindbox.isSecretOpen ? '0' : blindbox.priceIncreasePercent}%
+                    </div>
+                    <div className={`text-sm px-3 py-1 rounded-md bg-gradient-to-r from-green-400 to-blue-500`}>
+                      Turn {blindbox.turn}/{blindbox.maxTurn}
+                    </div>
+                  </div>
+                  
                 </div>
               </div>
 
               {/* Button - Foreground */}
               <div className="absolute bottom-20 z-40">
-                <Button onClick={paymentHandler}>
+                <motion.button 
+                  className=" p-1 px-6 rounded-2xl bg-gradient-to-tl from-amber-500 to-yellow-400" 
+                  onClick={paymentHandler}
+                  whileHover={{ scale: 1.1 }}
+                  whileTap={{ scale: 0.9 }}
+                  >
                   {blindbox.boxOption.displayPrice}
-                </Button>
+                </motion.button>
               </div>
 
               <div className="h-full w-full absolute z-30 flex flex-col items-center">
